a3a5a554060f3c0a76efd8826884c3cced9f6630
Ahmed Asfah <44223597+Asfah@users.noreply.github.com>
2019-09-05 17:14:28 +0500

    Update chatbot-specialchar.py

--- chatbot-specialchar.py
+++ chatbot-specialchar.py
@@ -1,1 +1,16 @@
+import fbchat 
+from getpass import getpass 
+
+username = "Your username"
+
+client = fbchat.Client(username, getpass()) 
+no_of_friends = int(raw_input("Number of friends: ")) 
+for i in xrange(no_of_friends): 
+    name = str(raw_input("Name: ")) 
+    friends = client.getUsers(name)  # return a list of names 
+    friend = friends[0] 
+    msg = str(raw_input("Message: ")) 
+    sent = client.send(friend.uid, msg) 
+    if sent: 
+        print("Message sent successfully!")
 
