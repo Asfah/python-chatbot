608f69bc8051b6f514880917f35cce4af3df0900
Ahmed Asfah <44223597+Asfah@users.noreply.github.com>
2019-09-08 21:13:08 +0500

    Update chat3.py

--- chat3.py
+++ chat3.py
@@ -3,18 +3,18 @@ from getpass import getpass
 from fbchat.models import *
 
 
-username = str(input("Username: "))
-client = Client(username, getpass())
+username = str(input("Username: "))   #Enter phone number or username
+client = Client(username, getpass())   #Enter the password
 
-n = int(input("Enter number of bulk message: "))
+n = int(input("Enter number of bulk message: ")) #Entre the number of messages you want to send
 
-name = str(input("Enter frind name: "))
+name = str(input("Enter frind name: "))     #Entr name of the friend ou want to send the message to
 friend = client.searchForUsers(name)[0]
 
 if friend.name == name:
   print(friend.name)
 
-msg = str(input("Enter message to send: "))
+msg = str(input("Enter message to send: "))     #Enter the message
 
 for i in range (n+1):
  sent = client.send(Message(text=msg), friend.uid)
